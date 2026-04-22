50d146b3a5bd8f0a98451260ce538025c47d0c3b
wong_yx <wongyongxiang00@gmail.com>
2026-04-22 23:28:25 +0800

    Initialise providers.tf

---
+++ terraform/providers.tf
@@ -0,0 +1,16 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+    cloudflare = {
+      source  = "cloudflare/cloudflare"
+      version = "~> 4.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = var.aws_region
+}
