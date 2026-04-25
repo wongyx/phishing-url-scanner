c7d6cc1523d5f4a2dcb4330170bd397ed421fa31
wong_yx <wongyongxiang00@gmail.com>
2026-04-25 19:40:10 +0800

    Add outputs for eks

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -22,3 +22,39 @@ output "nat_gateway_public_ip" {
   description = "Elastic IP attached to the NAT Gateway"
   value       = aws_eip.nat.public_ip
 }
+
+output "cluster_name" {
+  description = "EKS cluster name"
+  value       = aws_eks_cluster.main.name
+}
+
+output "cluster_endpoint" {
+  description = "EKS cluster API server endpoint"
+  value       = aws_eks_cluster.main.endpoint
+}
+
+output "cluster_ca_certificate" {
+  description = "Base64-encoded cluster CA certificate"
+  value       = aws_eks_cluster.main.certificate_authority[0].data
+  sensitive   = true
+}
+
+output "cluster_version" {
+  description = "Kubernetes version running on the cluster"
+  value       = aws_eks_cluster.main.version
+}
+
+output "oidc_provider_arn" {
+  description = "ARN of the OIDC provider (use this when creating IRSA roles)"
+  value       = aws_iam_openid_connect_provider.eks.arn
+}
+
+output "oidc_provider_url" {
+  description = "URL of the OIDC provider (without https://)"
+  value       = replace(aws_iam_openid_connect_provider.eks.url, "https://", "")
+}
+
+output "node_role_arn" {
+  description = "ARN of the IAM role attached to worker nodes"
+  value       = aws_iam_role.eks_node.arn
+}
