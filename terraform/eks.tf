bdef7d48df7a6290ab6a599d4bfd06b6b3657183
wong_yx <wongyongxiang00@gmail.com>
2026-04-25 19:47:29 +0800

    Remove unnecessaary security group for eks cluster

--- terraform/eks.tf
+++ terraform/eks.tf
@@ -1,32 +1,31 @@
 data "aws_eks_addon_version" "latest" {
-    for_each = toset(["vpc-cni", "coredns", "kube-proxy"])
+  for_each = toset(["vpc-cni", "coredns", "kube-proxy"])
 
-    addon_name = each.key
-    kubernetes_version = aws_eks_cluster.main.version
-    most_recent = true
+  addon_name         = each.key
+  kubernetes_version = aws_eks_cluster.main.version
+  most_recent        = true
 }
 
 resource "aws_eks_cluster" "main" {
-  name = var.cluster_name
+  name    = var.cluster_name
   version = "1.35"
 
   role_arn = aws_iam_role.eks_cluster.arn
 
   vpc_config {
     subnet_ids = concat(
-        [aws_subnet.private[0].id, aws_subnet.private[1].id],
-        [aws_subnet.public[0].id, aws_subnet.public[1].id]
+      [aws_subnet.private[0].id, aws_subnet.private[1].id],
+      [aws_subnet.public[0].id, aws_subnet.public[1].id]
     )
 
-    security_group_ids = [aws_security_group.eks_cluster.id]
-    endpoint_public_access = true
+    endpoint_public_access  = true
     endpoint_private_access = true
   }
 
   enabled_cluster_log_types = ["api", "audit", "authenticator", "controllerManager", "scheduler"]
 
   access_config {
-    authentication_mode = "API_AND_CONFIG_MAP"
+    authentication_mode                         = "API_AND_CONFIG_MAP"
     bootstrap_cluster_creator_admin_permissions = true
   }
 
@@ -41,28 +40,28 @@ resource "aws_eks_cluster" "main" {
 }
 
 resource "aws_cloudwatch_log_group" "eks" {
-    name = "/aws/eks/${var.cluster_name}/cluster"
-    retention_in_days = 7
+  name              = "/aws/eks/${var.cluster_name}/cluster"
+  retention_in_days = 7
 
-    tags = {
-        Name = "${var.cluster_name}-cloudwatch"
-    }
+  tags = {
+    Name = "${var.cluster_name}-cloudwatch"
+  }
 }
 
 resource "aws_eks_node_group" "main" {
-  cluster_name = aws_eks_cluster.main.name
+  cluster_name    = aws_eks_cluster.main.name
   node_group_name = "${var.cluster_name}-main-ng"
-  node_role_arn = aws_iam_role.eks_node.arn
-  subnet_ids = aws_subnet.private[*].id
-  
+  node_role_arn   = aws_iam_role.eks_node.arn
+  subnet_ids      = aws_subnet.private[*].id
+
   ami_type = "AL2023_x86_64_STANDARD"
 
   instance_types = ["t3.small"]
 
   scaling_config {
     desired_size = 1
-    min_size = 1
-    max_size = 2
+    min_size     = 1
+    max_size     = 2
   }
 
   update_config {
@@ -77,36 +76,36 @@ resource "aws_eks_node_group" "main" {
     ignore_changes = [scaling_config[0].desired_size]
   }
 
-  depends_on = [ 
+  depends_on = [
     aws_iam_role_policy_attachment.eks_worker_node_policy,
-   ]
+  ]
 
-   tags = {
+  tags = {
     Name = "${var.cluster_name}-main-ng"
-   }
+  }
 }
 
 resource "aws_eks_addon" "vpc_cni" {
-  cluster_name = aws_eks_cluster.main.name
-  addon_name = "vpc-cni"
+  cluster_name  = aws_eks_cluster.main.name
+  addon_name    = "vpc-cni"
   addon_version = data.aws_eks_addon_version.latest["vpc-cni"].version
 
-  service_account_role_arn = aws_iam_role.vpc_cni.arn
+  service_account_role_arn    = aws_iam_role.vpc_cni.arn
   resolve_conflicts_on_update = "OVERWRITE"
 }
 
 resource "aws_eks_addon" "coredns" {
-  cluster_name = aws_eks_cluster.main.name
-  addon_name = "coredns"
+  cluster_name  = aws_eks_cluster.main.name
+  addon_name    = "coredns"
   addon_version = data.aws_eks_addon_version.latest["coredns"].version
 
   resolve_conflicts_on_update = "OVERWRITE"
-  depends_on = [aws_eks_node_group.main]
+  depends_on                  = [aws_eks_node_group.main]
 }
 
 resource "aws_eks_addon" "kube_proxy" {
-  cluster_name = aws_eks_cluster.main.name
-  addon_name = "kube-proxy"
+  cluster_name  = aws_eks_cluster.main.name
+  addon_name    = "kube-proxy"
   addon_version = data.aws_eks_addon_version.latest["kube-proxy"].version
 
   resolve_conflicts_on_update = "OVERWRITE"
@@ -114,17 +113,17 @@ resource "aws_eks_addon" "kube_proxy" {
 
 # OIDC Provider for IRSA
 data "tls_certificate" "eks_oidc" {
-    url = aws_eks_cluster.main.identity[0].oidc[0].issuer
+  url = aws_eks_cluster.main.identity[0].oidc[0].issuer
 }
 
 resource "aws_iam_openid_connect_provider" "eks" {
-    url = aws_eks_cluster.main.identity[0].oidc[0].issuer
-    client_id_list = ["sts.amazonaws.com"]
-    thumbprint_list = [
-        data.tls_certificate.eks_oidc.certificates[0].sha1_fingerprint
-    ]
+  url            = aws_eks_cluster.main.identity[0].oidc[0].issuer
+  client_id_list = ["sts.amazonaws.com"]
+  thumbprint_list = [
+    data.tls_certificate.eks_oidc.certificates[0].sha1_fingerprint
+  ]
 
-    tags = {
-        Name = "${var.cluster_name}-oidc-provider"
-    }
+  tags = {
+    Name = "${var.cluster_name}-oidc-provider"
+  }
 }
