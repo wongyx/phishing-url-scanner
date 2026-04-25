b1c32848a301f629195701019e7ed8fd35562f9c
wong_yx <wongyongxiang00@gmail.com>
2026-04-25 19:56:55 +0800

    Linting

--- terraform/iam.tf
+++ terraform/iam.tf
@@ -1,41 +1,41 @@
 # Policy for EKS cluster
 data "aws_iam_policy_document" "eks_cluster_assume_role" {
-    statement {
-      effect = "Allow"
-      actions = ["sts:AssumeRole"]
+  statement {
+    effect  = "Allow"
+    actions = ["sts:AssumeRole"]
 
-      principals {
-        type = "Service"
-        identifiers = ["eks.amazonaws.com"]
-      }
+    principals {
+      type        = "Service"
+      identifiers = ["eks.amazonaws.com"]
     }
+  }
 }
 
 resource "aws_iam_role" "eks_cluster" {
-  name = "${var.cluster_name}-cluster-role"
+  name               = "${var.cluster_name}-cluster-role"
   assume_role_policy = data.aws_iam_policy_document.eks_cluster_assume_role.json
 }
 
 resource "aws_iam_role_policy_attachment" "eks_cluster_policy" {
-  role = aws_iam_role.eks_cluster.name
+  role       = aws_iam_role.eks_cluster.name
   policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
 }
 
 # Policy for EKS Node Group
 data "aws_iam_policy_document" "eks_node_assume_role" {
-    statement {
-      effect = "Allow"
-      actions = ["sts:AssumeRole"]
+  statement {
+    effect  = "Allow"
+    actions = ["sts:AssumeRole"]
 
-      principals {
-        type = "Service"
-        identifiers = ["ec2.amazonaws.com"]
-      }
+    principals {
+      type        = "Service"
+      identifiers = ["ec2.amazonaws.com"]
     }
+  }
 }
 
 resource "aws_iam_role" "eks_node" {
-  name = "${var.cluster_name}-node-role"
+  name               = "${var.cluster_name}-node-role"
   assume_role_policy = data.aws_iam_policy_document.eks_node_assume_role.json
 }
 
@@ -47,22 +47,22 @@ resource "aws_iam_role_policy_attachment" "eks_worker_node_policy" {
 # IAM Role for VPC CNI addon
 data "aws_iam_policy_document" "vpc_cni_assume_role" {
   statement {
-    effect = "Allow"
+    effect  = "Allow"
     actions = ["sts:AssumeRoleWithWebIdentity"]
 
     principals {
-      type = "Federated"
+      type        = "Federated"
       identifiers = [aws_iam_openid_connect_provider.eks.arn]
     }
 
     condition {
-      test = "StringEquals"
+      test     = "StringEquals"
       variable = "${replace(aws_iam_openid_connect_provider.eks.url, "https://", "")}:sub"
-      values = ["system:serviceaccount:kube-system:aws-node"]
+      values   = ["system:serviceaccount:kube-system:aws-node"]
     }
 
     condition {
-      test = "StringEquals"
+      test     = "StringEquals"
       variable = "${replace(aws_iam_openid_connect_provider.eks.url, "https://", "")}:aud"
       values   = ["sts.amazonaws.com"]
     }
