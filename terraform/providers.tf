08c63deb2415acc4dbb2a6fb3f6683dfdccb7dbe
wong_yx <wongyongxiang00@gmail.com>
2026-04-30 22:34:01 +0800

    Add providers

--- terraform/providers.tf
+++ terraform/providers.tf
@@ -8,9 +8,25 @@ terraform {
       source  = "cloudflare/cloudflare"
       version = "~> 4.0"
     }
+    http = {
+      source  = "hashicorp/http"
+      version = "~> 3.0"
+    }
+    tls = {
+      source  = "hashicorp/tls"
+      version = "~> 4.0"
+    }
+    random = {
+      source  = "hashicorp/random"
+      version = "~> 3.0"
+    }
   }
 }
 
 provider "aws" {
   region = var.aws_region
+}
+
+provider "cloudflare" {
+  api_token = var.cloudflare_api_token
 }
